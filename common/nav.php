adb7ade50232f3bc4ea5b5b45258ce28843dd090
Jino Park <Jino Park@Jino-Ultrabook.(none)>
2013-12-23 21:36:09 -0700

    removed ct310 from nav

--- common/nav.php
+++ common/nav.php
@@ -4,11 +4,11 @@
 		<div class="nav_item_container">
 			<a class="nav_item" href="http://www.cs.colostate.edu/~jpark">HOME</a>
 		</div>
-
+<!--
 		<div class="nav_item_container">
 			<a class="nav_item" href="http://www.cs.colostate.edu/~jpark/CT310.php">CT 310</a>
 		</div>
-	
+-->
 		<div class="nav_item_container">
 			<a class="nav_item" href="http://www.cs.colostate.edu/~jpark/sandbox.php">SAND</a>
 		</div>
